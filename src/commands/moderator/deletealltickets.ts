1b71af367f08dd3e33b0fd0c0beb9a84cb86bd16
youngkesinova <youngkesinova@gmail.com>
2021-02-09 16:30:38 +0100

    Update deletealltickets.ts

--- src/commands/moderator/deletealltickets.ts
+++ src/commands/moderator/deletealltickets.ts
@@ -6,22 +6,22 @@ import { Message, GuildMember } from "discord.js";
 import { color } from "../../Config";
 
 export default class DeleteAllTickets extends Command {
-  public constructor() {
-    super("deletealltickets", {
-      aliases: ["deletealltickets"],
-      category: "Public Commands",
-      userPermissions: "MANAGE_MESSAGES",
-      ratelimit: 40,
-    });
-  }
+   public constructor() {
+      super("deletealltickets", {
+         aliases: ["deletealltickets"],
+         category: "Public Commands",
+         userPermissions: "MANAGE_MESSAGES",
+         ratelimit: 40,
+      });
+   }
 
-  public async exec(message: Message, { member }: { member: GuildMember }) {
-    const SupportChannel = message.guild.channels.cache.filter((name) =>
-      name.name.includes("tls-")
-    );
+   public async exec(message: Message, { member }: { member: GuildMember }) {
+      const SupportChannel = message.guild.channels.cache.filter((name) =>
+         name.name.includes("dls-")
+      );
 
-    if (SupportChannel) {
-      SupportChannel.map(async (ticket) => await ticket.delete());
-    } else return;
-  }
+      if (SupportChannel) {
+         SupportChannel.map(async (ticket) => await ticket.delete());
+      } else return;
+   }
 }
