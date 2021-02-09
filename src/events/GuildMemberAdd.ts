63fdd14273b21293a521bc87d4232cd736439f34
youngkesinova <youngkesinova@gmail.com>
2021-02-09 16:30:52 +0100

    add rem user

--- src/events/GuildMemberAdd.ts
+++ src/events/GuildMemberAdd.ts
@@ -8,44 +8,44 @@ import { DiscordAPIError, GuildMember } from "discord.js";
 import { BotVersion, color, PRODUCTION } from "../Config";
 
 export default class GuildMemberAdd extends Listener {
-  public constructor() {
-    super("guildMemberAdd", {
-      emitter: "client",
-      event: "guildMemberAdd",
-      category: "client",
-    });
-  }
+   public constructor() {
+      super("guildMemberAdd", {
+         emitter: "client",
+         event: "guildMemberAdd",
+         category: "client",
+      });
+   }
 
-  public async exec(member: GuildMember) {
-    const userThatJoined: GuildMember = await member.guild.members
-      .fetch(member.id)
-      .catch(() => null);
+   public async exec(member: GuildMember) {
+      const userThatJoined: GuildMember = await member.guild.members
+         .fetch(member.id)
+         .catch(() => null);
 
-    const welcomeChannel = await this.client.settings.get(
-      member.guild.id,
-      "config.welcomechannel"
-    );
+      const welcomeChannel = await this.client.settings.get(
+         member.guild.id,
+         "config.welcomechannel"
+      );
 
-    const channelToSend = member.guild.channels.cache.get(
-      welcomeChannel
-    ) as TextChannel;
+      const channelToSend = member.guild.channels.cache.get(
+         welcomeChannel
+      ) as TextChannel;
 
-    if (welcomeChannel && channelToSend) {
-      const embed = new MessageEmbed()
-        .setDescription(
-          `**Assalamu’alaikum (May Peace Be Upon You)**\n\nPlease type \`.apply\` to get this verification process started..`
-        )
-        .setColor(color)
-        .setAuthor(
-          userThatJoined.user.username,
-          userThatJoined.user.displayAvatarURL({
-            dynamic: true,
-            format: "png",
-          })
-        )
-        .setFooter(`TLS-${userThatJoined.id}`);
-      await channelToSend.send(`${userThatJoined.user}`).catch(() => null);
-      await channelToSend.send(embed).catch(() => null);
-    }
-  }
+      if (welcomeChannel && channelToSend) {
+         const embed = new MessageEmbed()
+            .setDescription(
+               `**Assalamu’alaikum (Moge vrede met u zijn)**\n\nTyp \`.apply\` om dit verificatieproces te starten..`
+            )
+            .setColor(color)
+            .setAuthor(
+               userThatJoined.user.username,
+               userThatJoined.user.displayAvatarURL({
+                  dynamic: true,
+                  format: "png",
+               })
+            )
+            .setFooter(`dls-${userThatJoined.id}`);
+         await channelToSend.send(`${userThatJoined.user}`).catch(() => null);
+         await channelToSend.send(embed).catch(() => null);
+      }
+   }
 }
--- src/events/GuildMemberRemove.ts
+++ src/events/GuildMemberRemove.ts
@@ -3,19 +3,19 @@ import { Listener } from "discord-akairo";
 import { GuildMember } from "discord.js";
 
 export default class GuildMemberRemove extends Listener {
-  public constructor() {
-    super("guildMemberRemove", {
-      emitter: "client",
-      event: "guildMemberRemove",
-      category: "client",
-    });
-  }
+   public constructor() {
+      super("guildMemberRemove", {
+         emitter: "client",
+         event: "guildMemberRemove",
+         category: "client",
+      });
+   }
 
-  public async exec(member: GuildMember) {
-    const ticketChannelSecure = member.guild.channels.cache.find(
-      (ticket) => ticket.name === `tls-${member.id}`
-    );
+   public async exec(member: GuildMember) {
+      const ticketChannelSecure = member.guild.channels.cache.find(
+         (ticket) => ticket.name === `dls-${member.id}`
+      );
 
-    if (ticketChannelSecure) await ticketChannelSecure.delete();
-  }
+      if (ticketChannelSecure) await ticketChannelSecure.delete();
+   }
 }
