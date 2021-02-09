9289797b28b05bf0662ec02873c27f5fbba69890
youngkesinova <youngkesinova@gmail.com>
2021-02-09 16:30:40 +0100

    Update deleteticket.ts

--- src/commands/moderator/deleteticket.ts
+++ src/commands/moderator/deleteticket.ts
@@ -6,37 +6,39 @@ import { Message, GuildMember } from "discord.js";
 import { color } from "../../Config";
 
 export default class DeleteTicket extends Command {
-  public constructor() {
-    super("deleteticket", {
-      aliases: ["deleteticket"],
-      category: "Public Commands",
-      userPermissions: "MANAGE_MESSAGES",
-      ratelimit: 40,
-      args: [
-        {
-          id: "member",
-          type: "member",
-          prompt: {
-            start: (msg: Message) => `${msg.author},\n\nverify [@member]`,
-            retry: (msg: Message) => `${msg.author},\n\nverify [@member]`,
-          },
-        },
-      ],
-    });
-  }
+   public constructor() {
+      super("deleteticket", {
+         aliases: ["deleteticket"],
+         category: "Public Commands",
+         userPermissions: "MANAGE_MESSAGES",
+         ratelimit: 40,
+         args: [
+            {
+               id: "member",
+               type: "member",
+               prompt: {
+                  start: (msg: Message) => `${msg.author},\n\nverify [@member]`,
+                  retry: (msg: Message) => `${msg.author},\n\nverify [@member]`,
+               },
+            },
+         ],
+      });
+   }
 
-  public async exec(message: Message, { member }: { member: GuildMember }) {
-    const SupportChannel = message.guild.channels.cache.find(
-      (name) => name.name === `tls-${member.id}`
-    );
+   public async exec(message: Message, { member }: { member: GuildMember }) {
+      const SupportChannel = message.guild.channels.cache.find(
+         (name) => name.name === `dls-${member.id}`
+      );
 
-    if (!SupportChannel)
-      return message.channel.send(`${member} doesnt have a ticket..`);
+      if (!SupportChannel)
+         return message.channel.send(`${member} doesnt have a ticket..`);
 
-    await SupportChannel.delete()
-      .then(() => {
-        return message.channel.send(`${member} ticket removed succesfully..`);
-      })
-      .catch(() => null);
-  }
+      await SupportChannel.delete()
+         .then(() => {
+            return message.channel.send(
+               `${member} ticket removed succesfully..`
+            );
+         })
+         .catch(() => null);
+   }
 }
