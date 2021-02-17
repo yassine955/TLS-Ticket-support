50cf5b4cdbafb9734d34d1344cfd0735b96db2aa
youngkesinova <youngkesinova@gmail.com>
2021-02-17 21:01:08 +0100

    Create embed.ts

---
+++ src/commands/moderator/embed.ts
@@ -0,0 +1,41 @@
+import { Command } from "discord-akairo";
+import { MessageEmbed } from "discord.js";
+import { TextChannel } from "discord.js";
+import { Message, GuildMember } from "discord.js";
+import { Repository } from "typeorm";
+
+import { color } from "../../Config";
+import { _MESSAGE_EMBED } from "../../lib/_MESSAGE_EMBED";
+import { Banlist } from "../../models/Banlist";
+
+export default class EmbedCommand extends Command {
+  public constructor() {
+    super("embed", {
+      aliases: ["embed"],
+      category: "Public Commands",
+      userPermissions: "MANAGE_MESSAGES",
+      ratelimit: 40,
+      args: [
+        {
+          id: "msg",
+          type: "string",
+          match: "rest",
+          prompt: {
+            start: (msg: Message) => `${msg.author},\n\nembed [msg]`,
+            retry: (msg: Message) => `${msg.author},\n\nembed [msg]`,
+          },
+        },
+      ],
+    });
+  }
+
+  public async exec(message: Message, { msg }: { msg: string }) {
+
+    return message.util.send(new MessageEmbed({
+      color,
+      description: msg
+    }))
+
+
+  }
+}
